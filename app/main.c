72a8a406dbd17304af331e6a796360da9c570f2a
Saul Blanco tejero <sblanco-@c1r3s3.42madrid.com>
2024-09-04 21:24:56 +0200

    feat(exit): add simple implementation

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 
 static int strlen_until(char *str, char c)
@@ -11,17 +12,37 @@ static int strlen_until(char *str, char c)
   return len;
 }
 
+static bool starts_with(char *str, char *suffix)
+{
+  int i = 0;
+
+
+  while (str[i] && suffix[i] && str[i] == suffix[i])
+    i++;
+
+  return (str[i] == ' ' || str[i] == '\n');
+}
+
+static void handle_command(char *input)
+{
+  if (starts_with(input, "exit")) {
+    exit(0);
+  } else {
+    printf("%.*s: command not found\n", strlen_until(input, '\n'), input);
+  }
+}
+
 int main()
 {
+  char input[100];
+
   while (true)
   {
     printf("$ ");
     fflush(stdout);
 
-    // Wait for user input
-    char input[100];
     fgets(input, 100, stdin);
-    printf("%.*s: command not found\n", strlen_until(input, '\n'), input);
+    handle_command(input);
   }
   return 0;
 }
