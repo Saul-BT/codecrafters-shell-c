2808c7e44c3d1ec38d52f721c107c02c6eb2afe4
Saul Blanco tejero <sblanco-@c1r3s3.42madrid.com>
2024-09-04 21:02:24 +0200

    fix: add dummy handle missing commands

--- app/main.c
+++ app/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+static int strlen_until(char *str, char c)
+{
+  int len = 0;
+
+  while (str[len] != c)
+    len++;
+
+  return len;
+}
+
 int main() {
   printf("$ ");
   fflush(stdout);
@@ -7,6 +17,6 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
-  printf("%s: command not found", input);
+  printf("%.*s: command not found", strlen_until(input, '\n'), input);
   return 0;
 }
