2085c34f81fbed913eaccc72b3748055f26211be
Saul Blanco tejero <sblanco-@c1r10s1.42madrid.com>
2024-09-05 08:51:09 +0200

    feat(cmd_type): handle path bins

--- app/main.c
+++ app/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #define BUILTIN_COUNT 3
 
@@ -31,6 +32,18 @@ static bool starts_with(const char *str, char *suffix)
   return (str[i] == ' ' || str[i] == '\n');
 }
 
+static char *get_path_dirs(char **envp)
+{
+  int i = 0;
+
+  while (strncmp(envp[i], "PATH=", 5))
+    i++;
+  
+  char *path_dirs = envp[i] + 5;
+
+  return path_dirs;
+}
+
 static void cmd_exit(const char *input)
 {
   exit(0);
@@ -41,18 +54,38 @@ static void cmd_echo(const char *input)
   printf("%s", input + 5); // input + 5 (aka. input + strlen("echo") + 1)
 }
 
-static void cmd_type(const char *input)
+static void cmd_type(const char *input, char **envp)
 {
   const char *arg = input + 5; // input + 5 (aka. input + strlen("type") + 1)
   const int arg_len = (int)strlen(arg) - 1;
+  char *path_dirs = get_path_dirs(envp);
 
-  if (!is_builtin(arg))
+  if (is_builtin(arg))
   {
-    printf("%.*s: not found\n", arg_len, arg);
+    printf("%.*s is a shell builtin\n", arg_len, arg);
     return;
   }
 
-  printf("%.*s is a shell builtin\n", arg_len, arg);
+  const char *path_delim = ":";
+  char *path_dir = strtok(path_dirs, path_delim);
+
+  while (path_dir)
+  {
+    char tmp[1000];
+    strcpy(tmp, path_dir);
+    strcat(tmp, "/");
+    strncat(tmp, arg, strlen(arg) - 1);
+
+    if (access(tmp, F_OK) == 0)
+    {
+      printf("%.*s is %s\n", (int)strlen(arg) - 1, arg, tmp);
+      return;
+    }
+
+    path_dir = strtok(NULL, path_delim);
+  }
+
+  printf("%.*s: not found\n", arg_len, arg);
 }
 
 static void *builtins_map[BUILTIN_COUNT][2] = {
@@ -61,7 +94,7 @@ static void *builtins_map[BUILTIN_COUNT][2] = {
   { "type", &cmd_type },
 };
 
-static void handle_command(const char *input)
+static void handle_command(const char *input, char **envp)
 {
   int i = 0;
 
@@ -69,7 +102,7 @@ static void handle_command(const char *input)
   {
     if (starts_with(input, builtins_map[i][0]))
     {
-      ((void (*)(const char *))builtins_map[i][1])(input);
+      ((void (*)(const char *, char **))builtins_map[i][1])(input, envp);
       return;
     }
   }
@@ -77,7 +110,7 @@ static void handle_command(const char *input)
   printf("%.*s: command not found\n", (int)strlen(input) - 1, input);
 }
 
-int main()
+int main(int _argc, char **_argv, char **envp)
 {
   char input[100];
 
@@ -87,7 +120,7 @@ int main()
     fflush(stdout);
 
     fgets(input, 100, stdin);
-    handle_command((const char *)input);
+    handle_command((const char *)input, envp);
   }
   return 0;
 }
